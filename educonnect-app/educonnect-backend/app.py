7857cc37004f7173f779d6318a29bfe4e4bbf820
teeja <teejasvinibundhoo@gmail.com>
2025-12-16 16:10:43 +0400

    updated app.py

--- educonnect-app/educonnect-backend/app.py
+++ educonnect-app/educonnect-backend/app.py
@@ -2338,6 +2338,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    signal.alarm(0)
+   
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
