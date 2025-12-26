2ef448f768b822649412316cd490e7cd6ffd58f3
teeja-b <166813992+teeja-b@users.noreply.github.com>
2025-12-26 06:52:39 +0000

    commented,testing

--- educonnect-app/educonnect-backend/app.py
+++ educonnect-app/educonnect-backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import datetime, timedelta
