2ce3b43b5f9cc5281e170adb3cf9ddb5f9c6ea96
teeja-b <166813992+teeja-b@users.noreply.github.com>
2026-04-13 13:53:54 +0400

    Update app.py

--- educonnect-app/educonnect-backend/app.py
+++ educonnect-app/educonnect-backend/app.py
@@ -465,7 +465,7 @@ def test_fcm_direct(user_id):
                 android=fcm_messaging.AndroidConfig(
                     priority='high',
                     notification=fcm_messaging.AndroidNotification(
-                        channel_id='calls_v7',
+                        channel_id='messages',
                     )
                 ),
                 token=token_obj.token,
@@ -527,7 +527,7 @@ def send_fcm_notification(user_id, title, body, data=None, notification_type='ge
             android_config = fcm_messaging.AndroidConfig(
                 priority='high',
                 notification=fcm_messaging.AndroidNotification(
-                    channel_id='calls_v7',
+                    channel_id='calls',
                     priority='high',
                     default_vibrate_timings=True,
                     default_sound=False,
